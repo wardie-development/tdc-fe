e4bcba4a0b6b2f746d3a45cc4e74c2a2fe69b96a
luanafront <luana.lopes.rodrigues1@gmail.com>
2023-12-15 17:07:34 -0300

    styling adjustment

--- src/components/atoms/CompatibilityBrands.jsx
+++ src/components/atoms/CompatibilityBrands.jsx
@@ -2,13 +2,9 @@ import './styles/CompatibilityBrands.css'
 
 const CellphoneLine = ({cellphone, search}) => {
   const removeMappedBrands = c => c.replace("Galaxy", "").replace("Xiaomi", "").replace("Motorola", "").replace("Oppo", "").replace("Samsung", "")
-  
   const searchHighlight = `<span style='background-color: yellow; color: red'>${search}</span>`
-
   const isOnlyModel = ["Samsung", "Motorola", "Apple"].includes(cellphone.brand)
-  
   const mainTitle = isOnlyModel ? cellphone.model : `${cellphone.brand} ${cellphone.model}`
-  
   const cellphoneString = `<span style="font-weight: 500">${mainTitle.replace("Galaxy", "Samsung")}</span> – ${cellphone.compatibilities.map(removeMappedBrands).join(' – ')}`
 
   return (
